7bde28f25f7d64e3877c01d5b04ad80e74061df2
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-08-26 19:18:23 +0530

    menu bar

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Retail Inventory Management System");
+            System.out.println("1. Add Product");
+            System.out.println("2. Update Product");
+            System.out.println("3. Delete Product");
+            System.out.println("4. View Product");
+            System.out.println("5. Display All Products");
+            System.out.println("6. Generate Report");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Add Product selected");
+                    break;
+                case 2:
+                    System.out.println("Update Product selected");
+                    break;
+                case 3:
+                    System.out.println("Delete Product selected");
+                    break;
+                case 4:
+                    System.out.println("View Product selected");
+                    break;
+                case 5:
+                    System.out.println("Display All Products selected");
+                    break;
+                case 6:
+                    System.out.println("Generate Report selected");
+                    break;
+                case 7:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        } while (choice != 7);
+        sc.close();
+    }
+}
