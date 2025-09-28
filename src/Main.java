8d5f64266b7038ff9e37e6d68e43c3ece7e9f2bf
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-09-28 13:24:05 +0530

    feat(auth): Add password hide functionallity in Console
    - Added the password hide functionallity to hide the input password to enhance the security of the application

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.Console;
+
 import model.Admin;
 import model.Staff;
 import model.User;
@@ -9,35 +11,37 @@ public class Main {
     public static void main(String[] args) {
 
         // Scanner class to get user input
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         // Welcome message and login prompt
         System.out.println("Retail Inventory Management System");
         System.out.print("Enter your position(Admin/Staff):");
-        String position=sc.nextLine();
-        if(position.equals("Staff")){
-            int staffID=sc.nextInt();
+        String position = sc.nextLine();
+        if (position.equals("Staff")) {
+            int staffID = sc.nextInt();
         }
         System.out.print("Enter your username:");
-        String username=sc.nextLine();
-        System.out.print("Enter your password:");
-        String password=sc.nextLine();
+        String username = sc.nextLine();
+
+        //Console class to hide the password in console
+        Console console = System.console();
+        char[] passwordChars = console.readPassword("Enter your password:");
+        String password = new String(passwordChars);
+
 
         // Simple authentication - Role based Authentication
         User user;
         if (username.equals("admin") && password.equals("admin123")) {
-            
+
             //Runtime implementation based on user role
             user = new Admin(username, password);
             showAdminMenu((Admin) user);
-        } 
-        else if (username.equals("staff") && password.equals("staff123")) {
-            
+        } else if (username.equals("staff") && password.equals("staff123")) {
+
             //Runtime implementation based on user role
             user = new Staff(username, password);
             showStaffMenu((Staff) user);
-        } 
-        else {
+        } else {
             System.out.println("Invalid credentials!");
             return;
         }
@@ -48,7 +52,7 @@ public class Main {
     private static void showAdminMenu(Admin admin) {
         int choice;
         // Scanner class to get user input
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         do {
             System.out.println("\nAdmin Menu");
             System.out.println("1.Add Product");
@@ -92,7 +96,7 @@ public class Main {
 
         int choice;
         // Scanner class to get user input
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         do {
             System.out.println("\nStaff Menu");
             System.out.println("1.View Products");
